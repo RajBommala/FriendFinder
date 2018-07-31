9b4992ec8ac09f64305bdfddb9d00f82c5de2311
Bommala <basusmy@gmail.com>
2018-07-30 19:55:22 -0500

    express app

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friends = [
+    { name: 'John',
+    photo: 'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ] },
+  
+    { name: 'Brad',
+    photo: 'https://images.pexels.com/photos/936229/pexels-photo-936229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [ '2', '2', '2', '2', '2', '2', '2', '2', '2', '2' ] },
+  
+    { name: 'Nikki',
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPC078eR2MdislM2u4qWl7-QJS6uA1iyABd_YECR-RTr7sOjqD',
+    scores: [ '3', '3', '3', '3', '3', '3', '3', '3', '3', '3' ] }
+  ];
+  
+  module.exports = friends;
---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var htmlRoutes = require('./app/routing/htmlRoutes');
+var apiRoutes = require('./app/routing/apiRoutes');
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+// Routes
+app.use('/', htmlRoutes);
+app.use('/survey', htmlRoutes);
+app.get('/api/friends', apiRoutes);
+app.post('/api/friends', apiRoutes);
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
+});
